59cbe121c6d4418b6fed5fa89d48b039b4f31037
MIMminE <weluv.ww@gmail.com>
2025-12-10 13:12:52 +0900

    레스토랑 서비스 모듈 추가

---
+++ restaurant-service/src/test/java/nuts/study/msa_saga/restaurantservice/RestaurantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nuts.study.msa_saga.restaurantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
